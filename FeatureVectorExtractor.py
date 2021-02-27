762af4a43547599fe818834e67414adf6069475e
Alex Girshfeld <alex.girshfeld@gmail.com>
2021-02-27 12:13:01 +0200

    added ExtractLabel Method

--- FeatureVectorExtractor.py
+++ FeatureVectorExtractor.py
@@ -11,6 +11,11 @@ class FeatureVectorExtractor:
         self.dynamicTree = ET.parse(dynamicReportFilePath)
         self.featureVectorKeys = FeatureVectorList
 
+    def ExtractLabel(self):
+        sroot = self.staticTree.getroot()
+        malwareTag = sroot.findall('./malware')
+        return malwareTag[0].text
+
     def ExtractFeatureVector(self):
         featureVector = {}
         for key in self.featureVectorKeys:
@@ -51,6 +56,25 @@ class FeatureVectorExtractor:
         for e in entries:
             stringList.append(e.text)
         return stringList
+
+def test_ExtractLabel():
+    testSHA256 = '0000eaf36c9d3217bfe5b89e027f86fd2de80bf541df1cabb337149ebdf5f415'
+    FeatureVectorExtractorMock = FeatureVectorExtractor(testSHA256,[])
+    assert FeatureVectorExtractorMock.ExtractLabel() == 'yes'
+    testSHA256 = '0c0b5e0f7808b76b040ea433ca64dda1c4f635749b2ef61a21e41b6f96325512'
+    FeatureVectorExtractorMock = FeatureVectorExtractor(testSHA256,[])
+    assert FeatureVectorExtractorMock.ExtractLabel() == 'no'
+
+
+def test_ExtractFeatureVector():
+    testSHA256 = '0000eaf36c9d3217bfe5b89e027f86fd2de80bf541df1cabb337149ebdf5f415'
+    testCaseVector = {'APK file communicates over a network socket': True, 'android.net.wifi.WIFI_STATE_CHANGED': True,
+     'android.permission.WRITE_EXTERNAL_STORAGE': True, 'android/net/ConnectivityManager;->getActiveNetworkInfo': True,
+     'abcdefg': False, 'APK file has the ability to install other APK files': True}
+    FeatureVectorExtractorMock = FeatureVectorExtractor(testSHA256, testCaseVector)
+    extractedVector = FeatureVectorExtractorMock.ExtractFeatureVector()
+    for feature in testCaseVector.keys():
+        assert testCaseVector[feature] == extractedVector[feature]
 
 
 def test_checkEntryInTree():
