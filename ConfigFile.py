7490d748d9751fcf9dbb38157601c3d8881d18f6
nofarmazor <nofarmazor@gmail.com>
2021-03-08 13:57:20 +0200

    fix data prep

--- ConfigFile.py
+++ ConfigFile.py
@@ -7,6 +7,6 @@ os.chdir(dir_path)
 #config file for shared variables
 
 Malware_tag = './malware'
-Malware_dir = '../malware/'
-Benign_dir = '../benign/'
+Malware_dir = 'malware/'
+Benign_dir = 'benign/'
 Decision = 'Decision'
