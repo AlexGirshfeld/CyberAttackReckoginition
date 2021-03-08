35eba844bbe7edadc6cbf622d484a4347cf2ef1c
nofarmazor <nofarmazor@gmail.com>
2021-03-08 10:47:40 +0200

    add config

--- ConfigFile.py
+++ ConfigFile.py
@@ -1,1 +1,12 @@
-__author__ = 'nofar'
+import os
+#get the current code files directory and change the working dir
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+
+
+#config file for shared variables
+
+Malware_tag = './malware'
+Malware_dir = '../malware/'
+Benign_dir = '../benign/'
+Decision = 'Decision'
--- DataPrep.py
+++ DataPrep.py
@@ -1,1 +1,13 @@
-__author__ = 'nofar'
+from FeatureVectorExtractor import FeatureVectorExtractor
+import ModelTrainer
+
+#prep data:
+testSHA256 = '0000eaf36c9d3217bfe5b89e027f86fd2de80bf541df1cabb337149ebdf5f415'
+sample = FeatureVectorExtractor(testSHA256,[])
+sample.ExtractEntriesKeys()
+print (sample.entry_keys)
+
+
+def get_benign_sample
+
+
