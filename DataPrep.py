b4348eeb4498af06c701314b2da860ff765a83ce
nofarmazor <nofarmazor@gmail.com>
2021-03-08 20:43:16 +0200

    add csv files and selected features csv with ipynb notebook for selecting features

--- DataPrep.py
+++ DataPrep.py
@@ -1,50 +1,44 @@
 import os
-import numpy as np
-import ConfigFile
 from FeatureVectorExtractor import FeatureVectorExtractor
-from ModelTrainer import ModelTrainer
-import pickle
-#prep data:
-#testSHA256 = '0000eaf36c9d3217bfe5b89e027f86fd2de80bf541df1cabb337149ebdf5f415'
-#sample = FeatureVectorExtractor(testSHA256,[])
-#sample.ExtractEntriesKeys()
-#print (sample.entry_keys)
+import pandas as pd
 
-
-
-def get_samples_hash(sample_dir, type):
+def get_samples_hash(sample_dir, type, start, end):
     full_path = sample_dir + "\\" + type
     if os.path.isdir(full_path):
         files = []
-
         # r=root, d=directories, f = files
         for r, d, f in os.walk(full_path):
             for file in f:
                 files.append(file[0:-7]) #remove _da.xml or _sa.xml
         unique_files = list(dict.fromkeys(files))
-        return unique_files
+        sliced_hashes = unique_files[start:end]
+        return sliced_hashes
 
-def create_feature_vector_extractor_list(file_hashes, start, end):
-    sliced_hashes = file_hashes[start:end]
+def create_feature_vector_extractor_list(file_hashes):
     fve_list = []
-    for hash in sliced_hashes:
+    count_bl = 0
+    for hash in file_hashes:
         try:
             fve  = FeatureVectorExtractor(hash,[])
             fve.ExtractEntriesKeys()
-            fve_list.append(fve)
+            fve_list.append(fve) #add featureVectorExtractor object to list
         except:
             print(f"enter {hash} into blacklist")
+            count_bl = count_bl + 1
+    print(f"corrupted hash files number is  = {count_bl}")
     return fve_list
 
-def generate_full_fve_list(start, end):
-    #create feature vector extractor list from file samples
+def generate_fves_hashes(start, end):
+    #create feature vector extractor list from file samples and sliced hash list
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)
-    benign_hashes = get_samples_hash(dir_path,"benign")
-    benign_fve_list = create_feature_vector_extractor_list(benign_hashes, start ,end)
+    benign_hashes = get_samples_hash(dir_path,"benign", start, end)
+    benign_fve_list = create_feature_vector_extractor_list(benign_hashes)
 
-    malware_hashes = get_samples_hash(dir_path,"malware")
-    malware_fve_list = create_feature_vector_extractor_list(malware_hashes, start ,end)
+    malware_hashes = get_samples_hash(dir_path,"malware", start, end)
+    malware_fve_list = create_feature_vector_extractor_list(malware_hashes)
+
+    #combine benign and malware
     fve_list = benign_fve_list
     hash_list = benign_hashes
     hash_list.extend(malware_hashes)
@@ -52,20 +46,22 @@ def generate_full_fve_list(start, end):
     return fve_list, hash_list
 
 
-fveList, hashList = generate_full_fve_list(0,1000)
+fveList, hashList = generate_fves_hashes(0,1000)
 allFeatures = []
 for fve in fveList:
     allFeatures.extend(fve.entry_keys)
 
-features = list(dict.fromkeys(allFeatures))
-print(len(features))
+#save to csv files:
+print("save data to csv files")
+hash_dict = {'hash': hashList}
+df = pd.DataFrame(hash_dict)
+df.to_csv('hashes.csv', index=False)
 
-mt = ModelTrainer(hashList, features)
-print(mt.createDataFrame(hashList, features))
+feature_dict = {'feature' : allFeatures }
+df = pd.DataFrame(feature_dict)
+df.to_csv('features.csv', index=False)
 
-
-
-
+print("Finish successfully")
 
 
 
