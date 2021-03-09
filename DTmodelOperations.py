93770c31cae8a2118d0560d2ca3b439a1fb8d3d5
Alex Girshfeld <alex.girshfeld@gmail.com>
2021-03-09 21:43:20 +0200

    split the model operations into two files

--- DTmodelOperations.py
+++ DTmodelOperations.py
@@ -7,44 +7,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics.classification import accuracy_score
 from FeatureVectorExtractor import FeatureVectorExtractor
 from FeatureVectorExtractor import findRandomAPK
-from dbn import SupervisedDBNClassification
+from ModelTrainer import ModelTrainer
+import chefboost as chef
 
-
+data =  pd.read_csv(r'dfAfterCorr.csv')
+featureVector = list(data.columns)
+apkHashes = pd.read_csv('hashes.csv').values.tolist()
+apkHashesList =[]
+for a in apkHashes:
+    apkHashesList.append(a[0])
+ourModel= ModelTrainer(apkHashesList, featureVector)
 # Loading dataset
-data =  pd.read_csv(r'dfwithoutNA.csv')
-print(f"nan values:{data.isnull().sum().sum()}")
-data = 1*data
-data.dropna(inplace=True)
-print(f"nan values:{data.isnull().sum().sum()}")
-dataWithoutLabels = data.iloc[:,:-1]
-labels = data.iloc[:,-1:]
-
-labels = labels.Decision.map(dict(yes=1, no=0))
-X, Y = dataWithoutLabels.to_numpy(), labels.to_numpy()
 
 
-#digits = load_digits()
-#X, Y = digits.data, digits.target
+dtClassifier = ourModel.TrainDTree()
 
-
+print("pause")
 # Splitting data
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.8, random_state=0)
 
-
-# Training
-#classifier = SupervisedDBNClassification(hidden_layers_structure=[256, 256],learning_rate_rbm=0.05,learning_rate=0.1,n_epochs_rbm=10,n_iter_backprop=100,batch_size=32,activation_function='relu',dropout_p=0.2)
-#classifier.fit(X_train, Y_train)
-
-#classifier.save('DBNmodel.pkl')
-classifier = SupervisedDBNClassification.load('DBNmodel.pkl')
-# Test
-Y_pred = classifier.predict(X_test)
-
-print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
-
-
-
-
+chef.save_model(dtClassifier, "DTmodel.pkl")
 #validation with the original data:
 def run_validation_on_external_data(numOfapks, classifier):
     featureVector = list(data.columns)
@@ -52,6 +33,7 @@ def run_validation_on_external_data(numOfapks, classifier):
     numOfapkschecked = numOfapks
     falsePositive =0
     falsseNegative = 0
+    wrongApks = []
     mapDict = {'yes':1, 'no': 0}
     for i in range(numOfapks):
         apkHash = findRandomAPK()
@@ -61,22 +43,24 @@ def run_validation_on_external_data(numOfapks, classifier):
             if label == 'n/a':
                 numOfapkschecked = numOfapkschecked -1
                 continue
-            fv = fve.ExtractBinFeatureVector()
-            fvArray = np.array(list(fv.values()),dtype='float64')
-            fvArray = fvArray[:-1]
+            fv = fve.ExtractFeatureVector()
         except Exception as e:
             numOfapkschecked = numOfapkschecked-1
             continue
-        prediction =  classifier.predict(fvArray)
-        if prediction[0] == mapDict[label]:
+        prediction = chef.predict(dtClassifier, fv)
+        if prediction == label:
             correct = correct + 1
         else:
+            wrongApks.append(apkHash)
             if mapDict[label] == 1:
                 falsseNegative= falsseNegative +1
             if mapDict[label] == 0:
                 falsePositive = falsePositive +1
+    pd.DataFrame(wrongApks).to_csv('mishitsApsDT.csv', index=False)
     print(f"Accuracy is {correct/numOfapkschecked}")
     print(f"FP rate is {falsePositive/numOfapkschecked}")
     print(f"misditect rte is {falsseNegative/numOfapkschecked}")
 
-run_validation_on_external_data(1000, classifier)
+run_validation_on_external_data(1000, dtClassifier)
+
+
